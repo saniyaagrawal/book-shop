8ba4303ff9181d1d6e2bc4615297ca8c0d46dab0
saniyaagrawal <saniya.agrawal30@gmail.com>
2020-12-05 11:28:31 +0530

    new card on home page

--- src/Components/Home/MyCard.jsx
+++ src/Components/Home/MyCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import clsx from 'clsx';
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -9,19 +10,38 @@ import { Link } from "react-router-dom";
 import { IconButton } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import Avatar from '@material-ui/core/Avatar';
 import "./Card.css";
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import { red, blue } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    height: 300,
-    width: 300,
-    margin: 13
+    maxWidth: 345,
   },
   media: {
-    height: 150,
-    minHeight: 150
-  }
-});
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  // expand: {
+  //   transform: 'rotate(0deg)',
+  //   marginLeft: 'auto',
+  //   transition: theme.transitions.create('transform', {
+  //   duration: theme.transitions.duration.shortest,
+  //   }),
+  // },
+  // expandOpen: {
+  //   transform: 'rotate(180deg)',
+  // },
+  avatar: {
+    backgroundColor: blue[700],
+  },
+}));
 
 const MyCard = ({id, book, flag}) => {
   const classes = useStyles();
@@ -32,57 +52,64 @@ const MyCard = ({id, book, flag}) => {
 
   const edit_book = (e) => {
     e.preventDefault();
-    <Link to={`/add`} />
+    <Link to={`/add/${id}`} />
   };
 
   const delete_book = (e) => {
     e.preventDefault();
+    
   };
 
   return (
 
       <div>
         <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia className={classes.media} image={images[0]} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                <div
-                  style={{
-                    flex: 1,
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                  }}
-                >
-                  Book1
-                  {/* {flag ? <EditIcon style={{color:'blue', fontSize:'large'}} className='edit'/> : <div></div>} */}
-                  { flag && <div classname='ed_button'>
-                      <IconButton aria-label="edit">
-                        <EditIcon style={{color:'gray', fontSize:'large'}} onClick={edit_book}/>
-                      </IconButton>
-                      {/* <IconButton aria-label="delete"> */}
-                        <DeleteIcon onClick={delete_book} style={{color:'gray', fontSize:'large'}} hover={{}}/>
-                      {/* </IconButton> */}
-                  </div>}
-                </div>
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Harry Potter is a series of seven fantasy novels written by
-                British author J. K. Rowling.
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                <div
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div>J.k.Rowling</div>
-                  <div>Rs. 500</div>
-                </div>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </div>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            C
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <ShoppingCartIcon />
+          </IconButton>
+        }
+        title="Book Name"
+        subheader="Author Name"
+      />
+      <CardMedia
+        className={classes.media}
+        image={images[0]}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+        <div style={{display:'flex', justifyContent:'space-between', alignItems: 'center'}}>
+          <div>
+          <IconButton aria-label="add to favorites">
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <DeleteIcon />
+          </IconButton>
+          </div>
+          Rs.500
+        </div>
+      </CardContent>
+      {/* <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <EditIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <DeleteIcon />
+        </IconButton>
+      </CardActions> */}
+    </Card>
+  </div>
   );
 };
 export default MyCard;
